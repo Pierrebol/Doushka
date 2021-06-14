3821a081548cb8af10513d7cd29a011586ad0672
pierrebollack <pierre.bollack@edhec.com>
2021-06-14 17:46:24 +0200

    disguises controller creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'disguises/index'
+  get 'disguises/show'
+  get 'disguises/new'
+  get 'disguises/create'
+  get 'disguises/update'
+  get 'disguises/edit'
+  get 'disguises/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
