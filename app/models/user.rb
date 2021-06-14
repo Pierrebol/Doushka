42917845324e143dfb9fce90a0c1d316d3d0cb75
pierrebollack <pierre.bollack@edhec.com>
2021-06-14 16:48:38 +0200

    quick fix model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :disguises, :bookings
+  has_many :disguises
+  has_many :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
