758dc261f5e1e43fd410b0c629e82a869ef8061d
Paul Deze <paulo.dze1@gmail.com>
2021-06-15 18:02:29 +0200

    form-booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,10 +5,14 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @disguise = Disguise.find(params[:disguise_id])
+    @booking.disguise = @disguise
+    raise
     if @booking.save
       redirect_to booking_path(@booking)
     else
-      render 'new'
+      render "/disguise_path(disguise)"
     end
   end
 
@@ -16,5 +20,11 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
     @booking.destroy
     redirect_to bookings.path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
